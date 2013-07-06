d916bb6fbeeb8bb25616f9c558c31615894e852d
Russ Brown <poa02rb@yahoo.co.uk>
2013-07-06 15:57:11 +0100

    css reset, js

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text" href="style.css"/>
-	<link href='http://fonts.googleapis.com/css?family=Londrina+Shadow' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div class="wrapper">
+	<section class="translate-wrapper">
 
-		<h1> Tyke Translate</h1>
+		<h1 class="title"> tyke translate</h1>
 
 		<div class="translate-input">
-			<label for="translate">enter normal words</label>
-			<input type="text" id="translate"></input>
+			<input type="text" id="input" placeholder="normal words"></input>
 		</div>
 
-		<button>Yorkshire-ify me youth</button>
+		<button class="translate-button" id="translate"><h2>translate<h2></button>
 
 		<div class="translated-output">
-			<label for="translated">get funny words</label>
-			<input type="text" id="translated"></input>
+			<input type="text" id="output" placeholder="funny words"></input>
 		</div>
 
-	</div>
+	</section>
+
+	<section class="stream-wrapper">
+
+		<div id="jstwitter"></div>
+
+	</section>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>	
+	<script src="js/main.js"></script>
+	<script>
+		$(function () {
+	    // start jqtweet!
+	    JQTWEET.loadTweets();
+		});
+	</script>
 </body>
 </html>
