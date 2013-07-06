a8012353e02f1928d2c8ea7364645498f511e4e2
adeniszczyc <andymanutd123@gmail.com>
2013-07-06 15:15:18 +0100

    Updated JS files and data

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
 	
 	$input = $("#input");
 	$translate = $("#translate");
-	$output = $("#translated");
+	$output = $("#output");
 
 	arrays = {};
 	result = {};
+
+	function capitaliseFirstLetter(string) {
+    	return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 	var app = {
 		init: function() {
 			this.bindEvents();
@@ -17,22 +21,45 @@ $(document).ready(function() {
 		breakdown: function() {
 
 			var text = $input.val();
-			var words = text.split(" ");
+			var newtext = text;
+
+			var temp = text.split(/[\s.:]+/);
+			for (var i = 0; i < temp.length; i++) {
+				var capital = capitaliseFirstLetter(temp[i]);
+				newtext.replace(temp[i], capital);
+			}
+
+
+
+			var words = newtext.split(/[ ]+/);;
+			result.string = newtext;
 			app.translate(words);
 			
 		},
 		translate: function(words) {
+			console.log(words);
 			if (typeof arrays.english != 'undefined') {
-				console.log("jdsjf");
 				for (var i = 0; i < words.length; i++) {
-				    var item = words[i];
-				    if ($.inArray(item, arrays.english) > -1){
-				    	var key = $.inArray(item, arrays.english);
-				    	words[i] = arrays.yorkshire[key];
+				    var item = words[i].replace(/[^\w\s]/gi, '');
+				    if ($.inArray(item.toLowerCase(), arrays.english) > -1){
+
+
+
+
+				    	var key = $.inArray(item.toLowerCase(), arrays.english);
+				    	console.log((result.string).replace(" " + words[i], " " + arrays.yorkshire[key]));
+				    	if (item[0].toUpperCase() == item[0]) {
+				    		console.log(item);
+				    		result.string = (result.string).replace(item, capitaliseFirstLetter(arrays.yorkshire[key]));
+				    	} 
+				    	else {
+				    		result.string = (result.string).replace(" " + item, " " + arrays.yorkshire[key]);
+				    	}
+
 				    } 
 				}
 			}
-			$output.val(words.join(" "));
+			$output.val(result.string);
 		},
 		loadDatabase: function() {
 			
@@ -50,7 +77,6 @@ $(document).ready(function() {
 			});
 		}
 	}
-
 
 	app.init();
 });
