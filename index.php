ec7e854bc9e140eefdca481d7878f1a205fc29f2
Russ Brown <poa02rb@yahoo.co.uk>
2013-07-06 13:19:53 +0100

    First stab at front end for tyke translator

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text" href="style.css"/>
+	<link href='http://fonts.googleapis.com/css?family=Londrina+Shadow' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="wrapper">
+
+		<h1> Tyke Translate</h1>
+
+		<div class="translate-input">
+			<label for="translate">enter normal words</label>
+			<input type="text" id="translate"></input>
+		</div>
+
+		<button>Yorkshire-ify me youth</button>
+
+		<div class="translated-output">
+			<label for="translated">get funny words</label>
+			<input type="text" id="translated"></input>
+		</div>
+
+	</div>
+</body>
+</html>
